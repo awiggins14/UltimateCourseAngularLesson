0f4c48bfa4d2f59fa1127d1ed6ceb6f3b92307c1
Abigail Wiggins <rebtaylor14@gmail.com>
2020-06-11 13:30:54 -0500

    Http, Observables, Services

---
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -0,0 +1,43 @@
+import { Passenger } from './models/passenger.interface';
+
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+
+import { Injectable } from '@angular/core';
+
+const PASSENGER_API: string = '/api/passengers';
+
+@Injectable()
+export class PassengerDashboardService {
+    constructor(private http: Http) { }
+
+    getPassengers(): Promise<Passenger[]> {
+        return this.http
+        .get(PASSENGER_API)
+        .toPromise()
+        .then((response: Response) => response.json());
+    }
+
+    updatePassenger(passenger: Passenger): Promise<Passenger> {
+      let headers = new Headers({
+        'Content-Type': 'application/json'
+      });
+      let options = new RequestOptions({
+        headers: headers
+      });
+      return this.http
+      .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
+      .toPromise()
+      .then((response: Response) => response.json());
+    }
+
+    removePassenger(passenger: Passenger): Promise<Passenger> {
+      return this.http
+      .delete(`${PASSENGER_API}/${passenger.id}`)
+      .toPromise()
+      .then((response: Response) => response.json());
+    }
+
+}
